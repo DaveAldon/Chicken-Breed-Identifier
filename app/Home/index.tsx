792fc1ab33d8ff131428a71583694f9dba704ec4
David Crawford <crawford@mail.gvsu.edu>
2023-07-25 21:37:13 -0400

    Working state

--- app/Home/index.tsx
+++ app/Home/index.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-export default function HomeScreen() {
+export default function Home() {
   return (
     <View>
       <Text className="text-red">HomeScreen</Text>
